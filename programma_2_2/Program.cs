7459da9ac0faf544ea6008e61ba4fdab17f5dba4
Nikita Kuznetsov <nikitacomik@gmail.com>
2023-02-13 20:07:28 +0300

    Спросили имя про Никиту и ответили

--- programma_2_2/Program.cs
+++ programma_2_2/Program.cs
@@ -4,3 +4,16 @@ double NumberB = new Random().Next(1,10);
 Console.WriteLine(NumberB);
 Console.WriteLine(NumberA + NumberB);
 Console.WriteLine(NumberA / NumberB);
+
+Console.WriteLine("Введите имя: ");
+
+string username = Console.ReadLine();
+
+if(username == "Никита")
+{
+    Console.WriteLine("Ура, это же Никита");
+}
+else
+{
+    Console.WriteLine("Жаль, что это не Никита");
+}
